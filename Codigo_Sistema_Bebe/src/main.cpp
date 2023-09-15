4e81aab4b0c9753068fbb6ca9ea81313698c7693
Pedro Soethe Chagas <soethechagas@gmail.com>
2023-09-15 11:42:39 -0300

    Código Teste Bluetooth

---
+++ Codigo_Sistema_Bebe/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Adafruit_VL6180X.h>
+#include <SoftwareSerial.h>
+
+#define COMAND 9
+#define CONECT 8
+
+SoftwareSerial MyBlue(2,3); // RX|TX
+
+void setup() {
+  // put your setup code here, to run once:
+  MyBlue.begin(9600);
+  Serial.begin(9600);
+  pinMode(COMAND, OUTPUT);
+  pinMode(CONECT, INPUT);
+}
+
+void loop() {
+  //while()
+  // put your main code here, to run repeatedly:
+  if(MyBlue.available()){
+    //Serial.println(MyBlue.read());
+  }
+}
+
